60ec483370f467c738f0f2109597e3f62bc65a90
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2026-04-17 03:20:59 +0700

    refactor(modules): flatten pages/ and hooks/ into module roots

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import { AppLayout } from "@/components/layout/AppLayout";
 import { HomePage } from "@/modules/home/HomePage";
-import { QuizFormPage } from "@/modules/quiz/form/pages/QuizFormPage";
-import { QuizPlayerPage } from "@/modules/attempt/pages/QuizPlayerPage";
-import { QuizResultPage } from "@/modules/attempt/pages/QuizResultPage";
+import { QuizFormPage } from "@/modules/quiz/form/QuizFormPage";
+import { QuizPlayerPage } from "@/modules/attempt/QuizPlayerPage";
+import { QuizResultPage } from "@/modules/attempt/QuizResultPage";
 
 export const router = createBrowserRouter([
   {
